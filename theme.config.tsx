687fd16c144116e70189927ee1702d4a0848baaa
Hopscotch On Desktop <qpootle1@gmail.com>
2024-08-05 11:49:52 +1000

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,15 +2,13 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>Hopscotch On Deskrop</span>,
+  logo: <span>Hopscotch On Desktop</span>,
   project: {
     link: 'https://github.com/HopscotchOnDesktop/HopscotchOnDesktop',
   },
+  docsRepositoryBase: 'https://github.com/HopscotchOnDesktop/docs',
   feedback: {
     content: null
-  },
-  editLink: {
-    component: null
   },
   footer: {
     component: null
